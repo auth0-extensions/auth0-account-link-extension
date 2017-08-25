f41c6cd46239ae887b94db1a59d5043348c952a7
Matthew Machuga <mmachuga@thinkthroughmath.com>
2017-08-25 06:44:07 -0400

    Adding error handling to UI
    If an invalid token comes in we notify the user that the state is
    invalid and ask them to try again. There is no UI button because we
    don't know where they came from, and using the browser history to go
    back will likely leave them in a broken state in the flow.
    
    If they have been redirected by the rule back to this point, then they
    likely tried to link accounts and accidentally chose an account with a
    mismatched email (like a second gmail account or a friend's facebook).

--- api/get_index.js
+++ api/get_index.js
@@ -910,6 +910,7 @@ module.exports = (server) => ({
         font-size: 11px;
         font-weight: 600;
         text-transform: uppercase;
+        display: none;
       }
       .auth0-lock.auth0-lock .auth0-global-message.auth0-global-message-error {
         background: #ff3e00;
@@ -2224,6 +2225,7 @@ module.exports = (server) => ({
                     <div class="auth0-lock-name">Link Accounts</div>
                   </div>
                 </div>
+                <div id="error-message" class="auth0-global-message auth0-global-message-error"></div>
                 <div style="position: relative;">
                   <span>
                     <div class="">
@@ -2232,9 +2234,9 @@ module.exports = (server) => ({
                           <div style="position: relative;">
                             <div class="auth0-lock-body-content">
                               <div class="auth0-lock-content">
-                                <div class="auth0-lock-form">
+                                <div class="auth0-lock-form" id="content-container">
                                   <div>
-                                    <p>
+                                    <p id="message">
                                       It looks like you have another account
                                       with the same email address. We recommend
                                       you link these accounts.
@@ -2257,7 +2259,7 @@ module.exports = (server) => ({
                 <div class="auth0-lock-actions">
                   <button class="auth0-lock-submit" type="button" id="link">
                     <span class="auth0-label-submit">
-                      Continue
+                      <span id="label-value">Continue</span>
                       <span>
                         <svg class="icon-text" width="8px" height="12px" viewBox="0 0 8 12" version="1.1" xmlns="http://www.w3.org/2000/svg"><g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="Web/Submit/Active" transform="translate(-148.000000, -32.000000)" fill="#FFFFFF"><polygon id="Shape" points="148 33.4 149.4 32 155.4 38 149.4 44 148 42.6 152.6 38"></polygon></g></g></svg>
                       </span>
@@ -2274,51 +2276,108 @@ module.exports = (server) => ({
     <script src="https://unpkg.com/jwt-decode@2.2.0/build/jwt-decode.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/qs/6.5.0/qs.min.js"></script>
     <script type="text/javascript">
-      var linkEl = document.getElementById('link');
-      var skipEl = document.getElementById('skip');
       var params = window.Qs.parse(window.location.search, { ignoreQueryPrefix: true });
-      var token = window.jwt_decode(params.child_token);
-      var connections = token.targetUsers.reduce(function(acc, user) {
-        return acc.concat(user.connections);
-      }, []);
 
-      var authorize = function(domain, qs) {
-        var query = Object.keys(qs)
-          .filter(function(key) {
-            return !!qs[key];
-          })
-          .map(function (key) {
-            return key + '=' + encodeURI(qs[key]);
-          }).join('&');
+      try {
+        loadLinkPage(window.jwt_decode(params.child_token));
+      } catch(e) {
+        console.error(e);
+        loadInvalidTokenPage();
+      }
 
-        window.location = domain + 'authorize?' + query;
-      };
+      function loadLinkPage(token) {
+        var linkEl = document.getElementById('link');
+        var skipEl = document.getElementById('skip');
+        var connections = token.targetUsers.reduce(function(acc, user) {
+          return acc.concat(user.connections);
+        }, []);
 
-      var updateContinueUrl = function(linkEl, domain, state) {
-        console.log("continue url is", domain + 'continue?state=' + state);
-        linkEl.href = domain + 'continue?state=' + state;
-      };
+        var authorize = function(domain, qs) {
+          var query = Object.keys(qs)
+            .filter(function(key) {
+              return !!qs[key];
+            })
+            .map(function (key) {
+              return key + '=' + encodeURI(qs[key]);
+            }).join('&');
 
-      console.log(params);
-      console.log(token);
-      console.log(connections);
-      console.log("States are", params.state, params.original_state);
+          window.location = domain + 'authorize?' + query;
+        };
 
-      linkEl.addEventListener('click', function(e) {
-        console.log("Link clicked");
-        authorize(token.iss, {
-          client_id: params.client_id,
-          redirect_uri: params.redirect_uri,
-          response_type: params.response_type,
-          scope: params.scope,
-          state: params.original_state,
-          nonce: params.nonce,
-          link_account_token: params.child_token,
-          connection: connections[0]
+        var updateContinueUrl = function(linkEl, domain, state) {
+          console.log("continue url is", domain + 'continue?state=' + state);
+          linkEl.href = domain + 'continue?state=' + state;
+        };
+
+        console.log(params);
+        console.log(token);
+        console.log(connections);
+        console.log("States are", params.state, params.original_state);
+
+        linkEl.addEventListener('click', function(e) {
+          console.log("Link clicked");
+          authorize(token.iss, {
+            client_id: params.client_id,
+            redirect_uri: params.redirect_uri,
+            response_type: params.response_type,
+            scope: params.scope,
+            state: params.original_state,
+            nonce: params.nonce,
+            link_account_token: params.child_token,
+            connection: connections[0]
+          });
         });
-      });
 
-      updateContinueUrl(skipEl, token.iss, params.state);
+        updateContinueUrl(skipEl, token.iss, params.state);
+
+        if (params.errorType === 'accountMismatch') {
+          loadAccountMismatchError();
+        }
+      }
+
+      function loadInvalidTokenPage() {
+        var containerEl = document.getElementById('content-container');
+        var labelEl = document.getElementById('label-value');
+        var linkEl = document.getElementById('link');
+
+        containerEl.innerHTML = '';
+        containerEl.appendChild(
+          el('div', {}, [
+            el('p', {}, [
+              text('You seem to have reached this page in error. Please try logging in again')
+            ])
+          ])
+        );
+
+        linkEl.disabled = true;
+      };
+
+      function loadAccountMismatchError() {
+        var messageEl = document.getElementById('error-message');
+        var msg = "Accounts must have matching email addresses. Please try again.";
+
+        messageEl.innerHTML = msg;
+        messageEl.style.display = 'block';
+      };
+
+      function el(tagName, attrs, childEls) {
+        var element = document.createElement(tagName);
+        var children = childEls || [];
+
+        for (var i in Object.keys(attrs || {})) {
+          element.setAttribute(i, attrs[i]);
+        }
+
+        for (var i in children) {
+          element.appendChild(children[i]);
+        }
+
+        return element;
+      }
+
+      function text(content) {
+        return document.createTextNode(content);
+      }
     </script>
   </body>
 </html>
