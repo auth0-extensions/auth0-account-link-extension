fa60d9d66fa0448dcfcb238dede28a36677cecb0
Franco Correa <fcorrea@sophilabs.com>
2017-10-09 16:04:04 -0300

    Skip linking accounts test case

--- test/puppeteer/index_test.js
+++ test/puppeteer/index_test.js
@@ -51,13 +51,22 @@ const deleteTestUsers = () =>
     });
   });
 
+const usersWithSameEmailCount = email =>
+  new Promise((resolve, reject) => {
+    mgmtApi.getUsers({ q: `email:"${email}"` }, (err, users) => {
+      if (err) return reject(err);
+
+      return resolve(users.length);
+    });
+  });
+
 describe('Account linking tests', () => {
-  before(async () => {
+  beforeEach(async () => {
     browser = await puppeteer.launch({ headless: false, width: 1366, height: 768 });
     page = await browser.newPage();
   });
 
-  after(async () => {
+  afterEach(async () => {
     browser.close();
 
     await deleteTestUsers().catch((e) => {
@@ -65,60 +74,8 @@ describe('Account linking tests', () => {
     });
   });
 
-  it('Rule detects repeated email', async () => {
-    await page.goto(app`/`);
-    await page.waitForSelector('#login-button');
-    await page.click('#login-button');
-    await page.waitForNavigation();
-
-    await wait(2);
-
-    await page.click(
-      '#auth0-lock-container-1 > div > div.auth0-lock-center > form > div > div > div:nth-child(3) > span > div > div > div > div > div > div > div > div > div.auth0-lock-tabs-container > ul > li:nth-child(2) > a'
-    );
-
-    await wait(0.5);
-
-    await page.waitForSelector('input[name="email"]');
-    await page.click('input[name="email"]');
-    await page.type(testEmail);
-
-    await page.click('input[name="password"]');
-    await page.type(testPassword);
-
-    await page.waitForSelector('.auth0-lock-submit');
-    await page.click('.auth0-lock-submit');
-
-    await page.waitForNavigation();
-    await page.click('#allow');
-
-    await page.waitForNavigation();
-    await page.click('#logout-button');
-    await page.waitForNavigation();
-    await page.click('#login-button');
-
-    await page.waitForNavigation();
-    await page.waitForSelector('.auth0-lock-alternative-link');
-    await wait(1);
-    await page.click('.auth0-lock-alternative-link');
-
-    await page.evaluate(() =>
-      document.querySelector('div.auth-lock-social-buttons-pane > div > button').click()
-    );
-
-    await page.waitForSelector('#identifierId');
-    await page.click('#identifierId');
-    await page.type(testEmail);
-
-    await page.click('#identifierNext');
-    await wait(1.5);
-
-    await page.click('input[name="password"]');
-    await page.type(testPassword);
-
-    await page.click('#passwordNext');
-
-    await page.waitForNavigation();
+  it('detects repeated email and links account', async () => {
+    await createUsers();
 
     await page.waitForSelector('#link');
     await page.click('#link');
@@ -148,6 +105,72 @@ describe('Account linking tests', () => {
     await wait(1);
 
     await page.waitForNavigation();
+    expect(await usersWithSameEmailCount(testEmail)).equal(1);
     expect(await page.url()).equal(app`/user`);
   });
+
+  it('skips linking', async () => {
+    await createUsers();
+
+    await page.evaluate(() => document.querySelector('#skip').click());
+
+    await page.waitForNavigation();
+    expect(await usersWithSameEmailCount(testEmail)).equal(2);
+  });
 });
+
+async function createUsers() {
+  await page.goto(app`/`);
+  await page.waitForSelector('#login-button');
+  await page.click('#login-button');
+  await page.waitForNavigation();
+
+  await wait(2);
+
+  await page.click(
+    '#auth0-lock-container-1 > div > div.auth0-lock-center > form > div > div > div:nth-child(3) > span > div > div > div > div > div > div > div > div > div.auth0-lock-tabs-container > ul > li:nth-child(2) > a'
+  );
+
+  await wait(0.5);
+
+  await page.waitForSelector('input[name="email"]');
+  await page.click('input[name="email"]');
+  await page.type(testEmail);
+
+  await page.click('input[name="password"]');
+  await page.type(testPassword);
+
+  await page.waitForSelector('.auth0-lock-submit');
+  await page.click('.auth0-lock-submit');
+
+  await page.waitForNavigation();
+  await page.click('#allow');
+
+  await page.waitForNavigation();
+  await page.click('#logout-button');
+  await page.waitForNavigation();
+  await page.click('#login-button');
+
+  await page.waitForNavigation();
+  await page.waitForSelector('.auth0-lock-alternative-link');
+  await wait(1);
+  await page.click('.auth0-lock-alternative-link');
+
+  await page.evaluate(() =>
+    document.querySelector('div.auth-lock-social-buttons-pane > div > button').click()
+  );
+
+  await page.waitForSelector('#identifierId');
+  await page.click('#identifierId');
+  await page.type(testEmail);
+
+  await page.click('#identifierNext');
+  await wait(1.5);
+
+  await page.click('input[name="password"]');
+  await page.type(testPassword);
+
+  await page.click('#passwordNext');
+
+  await page.waitForNavigation();
+}
