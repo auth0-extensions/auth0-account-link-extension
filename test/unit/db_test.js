b62a4a70285122c39a516796bdc791f155736d03
Franco Correa <fcorrea@sophilabs.com>
2017-11-03 15:22:57 -0300

    Add more unit tests

---
+++ test/unit/db_test.js
@@ -0,0 +1,18 @@
+import _ from 'lodash';
+import { expect } from 'chai';
+import { init, get } from '../../lib/db';
+
+describe('DB Tests', () => {
+  it('throws an error if I try to gey an nonexistent DB', () => {
+    init(null);
+    expect(() => get()).to.throw('The DB has not been initialized.');
+  });
+
+  it('saves and retrieves the DB object', () => {
+    const sampleStorageObject = { find: () => {}, save: () => {} };
+
+    init(sampleStorageObject);
+
+    expect(_.isEqual(sampleStorageObject, get())).to.equal(true);
+  });
+});
