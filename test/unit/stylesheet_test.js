111c34498ae03f2039b3e894ed691f947646b560
Matthew Machuga <mmachuga@thinkthroughmath.com>
2017-11-17 15:26:19 -0500

    Extracting stylesheet helper and linking admin css

---
+++ test/unit/stylesheet_test.js
@@ -0,0 +1,41 @@
+import { expect } from 'chai';
+import stylesheet from '../../lib/stylesheet';
+
+describe('Stylesheet helper', () => {
+  it('generates a tag when filename provided', () => {
+    const { tag } = stylesheet();
+    const result = tag('test');
+
+    expect(result).to.match(/^<link/);
+  });
+
+  it('creates proper href for filename', () => {
+    const { tag } = stylesheet();
+    const result = tag('test');
+
+    expect(result).to.match(/"\/css\/test\.css/);
+  });
+
+  it('generates nothing when no filename provided', () => {
+    const { tag } = stylesheet();
+    const result = tag('');
+
+    expect(result).to.be.empty;
+  });
+
+  describe('When using cdn', () => {
+    const { tag } = stylesheet(true);
+
+    it('adds CDN path to file', () => {
+      const result = tag('test');
+
+      expect(result).to.match(/"https:/);
+    });
+
+    it('links to minified version of file', () => {
+      const result = tag('test');
+
+      expect(result).to.match(/\/test\.min.css/);
+    });
+  });
+});
