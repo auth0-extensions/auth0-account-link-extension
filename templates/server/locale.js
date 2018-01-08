413dc8c326f76b0269bebae0cd2438034fbd6085
Franco Correa <franco@francocorrea.com.uy>
2018-01-08 16:12:17 -0300

    Create locale admin panel base template

---
+++ templates/server/locale.js
@@ -0,0 +1,37 @@
+import scripts from '../utils/scripts';
+import header from '../utils/header';
+
+export default ({ stylesheetTag, baseURL }) => `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <base href="${baseURL}"/>
+    <title>Auth0 Account Linking Extension Administrator</title>
+    <link rel="stylesheet" href="https://cdn.auth0.com/styleguide/core/2.0.5/core.min.css" />
+    <link rel="stylesheet" href="https://cdn.auth0.com/styleguide/components/2.0.0/components.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.31.0/codemirror.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.31.0/theme/material.min.css">
+    ${stylesheetTag}
+</head>
+<body>
+
+${header}
+
+<main class="container app-container">
+    <div class="row">
+        <div class="col-sm-4 list-group" id="locale-menu">
+        
+        </div>
+        <div class="col-sm-8" id="locale-detail">
+        </div>
+    </div>
+</main>
+
+
+${scripts}
+</body>
+</html>
+`;
