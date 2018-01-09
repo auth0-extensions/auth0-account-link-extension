76a1016088d8d1ccde9f537da61afc6186e01175
Franco Correa <franco@francocorrea.com.uy>
2018-01-09 14:38:50 -0300

    Include missing updates

--- server/routes.js
+++ server/routes.js
@@ -5,6 +5,7 @@ import metaRoute from '../api/get_meta';
 import getAdminIndex from '../api/admin/get_index';
 import getLocaleAdminIndex from '../api/admin/get_locale_index';
 import getLocales from '../api/admin/get_locales';
+import putLocales from '../api/admin/put_locales';
 import getAdminSettings from '../api/admin/get_settings';
 import putAdminSettings from '../api/admin/put_settings';
 import getUserDetails from '../api/admin/get_user_details';
@@ -22,6 +23,7 @@ const register = (server, options, next) => {
   createRoute(getUserDetails, server);
   createRoute(getLocaleAdminIndex, server);
   createRoute(getLocales, server);
+  createRoute(putLocales, server);
 
   next();
 };
