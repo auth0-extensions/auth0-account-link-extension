290d34dd9b250397f2ab455d19f0aa197014178e
Franco Correa <franco@francocorrea.com.uy>
2018-01-09 14:39:52 -0300

    Add JSON.stringify shim to fix ES6 transpilation ReferenceError when calling it directly from the stringified function

--- templates/utils/scripts.js
+++ templates/utils/scripts.js
@@ -1,7 +1,9 @@
 import adminJS from '../../public/admin';
 
 export default `
+<script>var JSONStringify = JSON.stringify;</script>
 <script src="https://code.jquery.com/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.31.0/codemirror.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.31.0/mode/xml/xml.js"></script>
