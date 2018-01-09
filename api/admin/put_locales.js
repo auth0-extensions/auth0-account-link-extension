7a9a289f0e6438962bcb2050847d6ace4cc071d1
Franco Correa <franco@francocorrea.com.uy>
2018-01-09 14:37:54 -0300

    Update locales endpoint

---
+++ api/admin/put_locales.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-useless-escape */
+
+import { setLocales } from '../../lib/storage';
+
+module.exports = () => ({
+  method: 'PUT',
+  config: {
+    auth: 'jwt'
+  },
+  path: '/admin/locales',
+  handler: (req, reply) => {
+    setLocales(req.payload).then((response) => {
+      reply(response);
+    });
+  }
+});
