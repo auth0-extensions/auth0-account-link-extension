75383e686dea01c3301e7a23cc834ef790ae1fa0
Franco Correa <franco@francocorrea.com.uy>
2018-01-17 10:51:11 -0300

    Add humanizer tests

---
+++ test/unit/humanize_test.js
@@ -0,0 +1,35 @@
+import _ from 'lodash';
+import { expect } from 'chai';
+import humanizeArray from '../../lib/humanize';
+
+describe('Humanizer Tests', () => {
+    it('represents an array of string as a readable sentence using defaults', () => {
+        const testArray = ['a', 'b', 'c'];
+        const result = humanizeArray(testArray)
+
+        expect(result).to.equal('a, b and c');
+    });
+
+    it('represents an array of string as a readable sentence using a given beforeLast', () => {
+        const testArray = ['a', 'b', 'c'];
+        const result = humanizeArray(testArray, 'or')
+
+        expect(result).to.equal('a, b or c');
+    });
+
+    it('represents an array of string as a readable sentence using given beforeLast and oxfordComma', () => {
+        const testArray = ['a', 'b', 'c'];
+        const result = humanizeArray(testArray, 'or', ',')
+
+        expect(result).to.equal('a, b, or c');
+    });
+
+    it('returns an empty string if the array is empty', () => {
+        expect(humanizeArray([])).to.equal('');
+    })
+
+    it('returns the first item of the array if it has one item', () => {
+        const result = humanizeArray(['foo']);
+        expect(result).to.equal('foo');
+    })
+});
