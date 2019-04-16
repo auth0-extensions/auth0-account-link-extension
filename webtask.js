4c439ff7927d87706bc8d78815fc8dec90b9e3bf
Alexander Zarubin <nonexist.void@gmail.com>
2019-04-16 12:05:16 +0300

    public wt url fix for stage2

--- webtask.js
+++ webtask.js
@@ -6,11 +6,15 @@ const config = require('./lib/config');
 const createServer = tools.createServer((wtConfig, wtStorage) => {
   logger.info('Starting Account Link Extension - Version:', process.env.CLIENT_VERSION);
   logger.info(' > WT_URL:', wtConfig('WT_URL'));
-  logger.info(' > PUBLIC_WT_URL:', config('PUBLIC_WT_URL'));
+  logger.info(' > PUBLIC_WT_URL:', wtConfig('PUBLIC_WT_URL'));
   return hapiApp(wtConfig, wtStorage);
 });
 
 module.exports = (context, req, res) => {
-  config.setValue('PUBLIC_WT_URL', tools.urlHelpers.getWebtaskUrl(req));
+  const publicUrl = (req.x_wt && req.x_wt.ectx && req.x_wt.ectx.PUBLIC_WT_URL) || false;
+  if (!publicUrl) {
+    config.setValue('PUBLIC_WT_URL', tools.urlHelpers.getWebtaskUrl(req));
+  }
+
   createServer(context, req, res);
 };
