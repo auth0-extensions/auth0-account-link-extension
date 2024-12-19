65f6041dfa776d94d7addc59b62d41fe96f88bc7
Bernard Wolff <2744510+bernardwolff@users.noreply.github.com>
2024-12-19 13:47:39 -0500

    revert server/index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 /* eslint-disable global-require */
 
 const path = require('path');
-const Hapi = require('@hapi/hapi');
-const Inert = require('@hapi/inert');
+const Hapi = require('@auth0/hapi');
+const Inert = require('inert');
 const jwt = require('hapi-auth-jwt2');
 const config = require('../lib/config');
 const logger = require('../lib/logger');
@@ -10,60 +10,65 @@ const routes = require('./routes');
 const defaultHandlers = require('./handlers');
 const auth = require('./auth');
 
-const createServer = async (handlers = defaultHandlers) => {
-  const server = Hapi.server({
+const createServer = (cb, handlers = defaultHandlers) => {
+  const server = new Hapi.Server();
+
+  server.connection({
     host: 'localhost',
     port: config('PORT'),
     routes: {
       cors: true,
+      validate: {},
       files: {
         relativeTo: path.join(__dirname, '../public')
       }
     }
   });
 
-  // Register plugins
-  await server.register([jwt, Inert]);
+  server.register([jwt, Inert], () => {});
 
-  // Define routes
-  server.route([
-    {
-      method: 'GET',
-      path: '/js/{file*}',
-      options: { auth: false },
-      handler: {
-        directory: {
-          path: path.join(__dirname, '../public/js')
-        }
-      }
-    },
-    {
-      method: 'GET',
-      path: '/css/{file*}',
-      options: { auth: false },
-      handler: {
-        directory: {
-          path: path.join(__dirname, '../public/css')
-        }
+  server.route({
+    method: 'GET',
+    path: '/js/{file*}',
+    config: { auth: false },
+    handler: {
+      directory: {
+        path: path.join(__dirname, '../public/js')
       }
     }
-  ]);
+  });
 
-  // Register additional plugins and routes
-  await server.register([auth, handlers, routes]);
+  server.route({
+    method: 'GET',
+    path: '/css/{file*}',
+    config: { auth: false },
+    handler: {
+      directory: {
+        path: path.join(__dirname, '../public/css')
+      }
+    }
+  });
 
-  // Configure server logging
-  logger.debug = (...args) => {
-    server.log(['debug'], args.join(' '));
-  };
-  logger.info = (...args) => {
-    server.log(['info'], args.join(' '));
-  };
-  logger.error = (...args) => {
-    server.log(['error'], args.join(' '));
-  };
+  server.register([auth, handlers, routes], (err) => {
+    // Use the server logger.
+    logger.debug = (...args) => {
+      server.log(['debug'], args.join(' '));
+    };
+    logger.info = (...args) => {
+      server.log(['info'], args.join(' '));
+    };
+    logger.error = (...args) => {
+      server.log(['error'], args.join(' '));
+    };
 
-  return server; // Return the server instance directly
+    if (err) {
+      cb(err);
+    }
+
+    cb(null, server);
+  });
+
+  return server;
 };
 
 module.exports = createServer;
