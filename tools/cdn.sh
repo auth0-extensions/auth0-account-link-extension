7eda4c5f41a02cd2ee4122796464545675da069d
Bernard Wolff <2744510+bernardwolff@users.noreply.github.com>
2025-01-06 21:27:29 +0000

    switch to github actions

--- tools/cdn.sh
+++ tools/cdn.sh
@@ -1,19 +1,19 @@
 CURRENT_VERSION=$(node tools/attribute.js version)
-EXTENSION_NAME="auth0-account-link-extension"
+EXTENSION_NAME="auth0-account-link"
 
-CDN_EXISTS=$(aws s3 ls s3://assets.us.auth0.com/css/$EXTENSION_NAME/$CURRENT_VERSION/ | grep "link.min.css")
-ADMIN_CDN_EXISTS=$(aws s3 ls s3://assets.us.auth0.com/css/$EXTENSION_NAME/$CURRENT_VERSION/ | grep "admin.min.css")
+CDN_EXISTS=$(aws s3 ls s3://assets.us.auth0.com/extensions/$EXTENSION_NAME/assets/ | grep "link.$CURRENT_VERSION.min.css")
+ADMIN_CDN_EXISTS=$(aws s3 ls s3://assets.us.auth0.com/extensions/$EXTENSION_NAME/assets/ | grep "admin.$CURRENT_VERSION.min.css")
 
 if [ ! -z "$CDN_EXISTS" ]; then
   echo "There is already a version $CURRENT_VERSION in the cdn. Skipping cdn publish…"
 else
-  aws s3 cp public/css/link.min.css s3://assets.us.auth0.com/extensions/$EXTENSION_NAME/$CURRENT_VERSION/link.min.css --region us-west-1 --quiet --cache-control "max-age=86400" --acl public-read
+  aws s3 cp dist/assets/link.$CURRENT_VERSION.min.css s3://assets.us.auth0.com/extensions/$EXTENSION_NAME/assets/link.$CURRENT_VERSION.min.css --region us-west-1 --cache-control "max-age=86400" --acl public-read
   echo "$CURRENT_VERSION uploaded to the cdn"
 fi
 
 if [ ! -z "$ADMIN_CDN_EXISTS" ]; then
   echo "There is already a version $CURRENT_VERSION in the cdn. Skipping cdn publish…"
 else
-  aws s3 cp public/css/admin.min.css s3://assets.us.auth0.com/extensions/$EXTENSION_NAME/$CURRENT_VERSION/admin.min.css --region us-west-1 --quiet --cache-control "max-age=86400" --acl public-read
+  aws s3 cp dist/assets/admin.$CURRENT_VERSION.min.css s3://assets.us.auth0.com/extensions/$EXTENSION_NAME/assets/admin.$CURRENT_VERSION.min.css --region us-west-1 --cache-control "max-age=86400" --acl public-read
   echo "$CURRENT_VERSION uploaded to the cdn"
 fi
