ba575e9188da5eadfc9d085fd5ad411e07160f1c
Ben <ben.gimbel@okta.com>
2025-04-07 11:44:32 -0500

    fix: more logic around majorminor deployment, and not upgrading version

--- tools/cdn.sh
+++ tools/cdn.sh
@@ -13,21 +13,21 @@ deploy_bundle() {
   CDN_EXISTS=$(aws s3 ls s3://assets.us.auth0.com/extensions/$EXTENSION_NAME/assets/ | grep "link.$1.min.css")
   ADMIN_CDN_EXISTS=$(aws s3 ls s3://assets.us.auth0.com/extensions/$EXTENSION_NAME/assets/ | grep "admin.$1.min.css")
 
-  if [ ! -z "$BUNDLE_EXISTS" ]; then
+  if [ ! -z "$BUNDLE_EXISTS" ] && [ "$1" == "$CURRENT_VERSION" ]; then
     echo "There is already a $EXTENSION_NAME-$1.js in the cdn. Skipping cdn publish…"
   else
     aws s3 cp build/bundle.js s3://assets.us.auth0.com/extensions/$EXTENSION_NAME/$EXTENSION_NAME-$1.js --region us-west-1 --acl public-read
     echo "$EXTENSION_NAME-$1.js uploaded to the cdn"
   fi
 
-  if [ ! -z "$CDN_EXISTS" ]; then
+  if [ ! -z "$CDN_EXISTS" ] && [ "$1" == "$CURRENT_VERSION" ]; then
     echo "There is already a link.$1.min.css in the cdn. Skipping cdn publish…"
   else
     aws s3 cp dist/assets/link.$1.min.css s3://assets.us.auth0.com/extensions/$EXTENSION_NAME/assets/link.$1.min.css --region us-west-1 --cache-control "max-age=86400" --acl public-read
     echo "link.$1.min.css uploaded to the cdn"
   fi
 
-  if [ ! -z "$ADMIN_CDN_EXISTS" ]; then
+  if [ ! -z "$ADMIN_CDN_EXISTS" ] && [ "$1" == "$CURRENT_VERSION" ]; then
     echo "There is already a admin.$1.min.css in the cdn. Skipping cdn publish…"
   else
     aws s3 cp dist/assets/admin.$1.min.css s3://assets.us.auth0.com/extensions/$EXTENSION_NAME/assets/admin.$1.min.css --region us-west-1 --cache-control "max-age=86400" --acl public-read
